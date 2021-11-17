e71dbd9ebbe0a17b39ff2070bb5a7d2ba36510e3
Mikko Kerava <mokso@iki.fi>
2021-11-17 21:34:24 +0200

    Refactored authentication

--- PSBeneAPI/Private/Invoke-BAPIAuthentication.ps1
+++ PSBeneAPI/Private/Invoke-BAPIAuthentication.ps1
@@ -1,9 +1,19 @@
-function Invoke-BAPIAuthentication {
+function Invoke-BapiAuthentication {
     param(
+        [Parameter(Mandatory = $true, ParameterSetName = "Cred")]
         [pscredential] $Credential,
+
         # Parameter help description
+        [Parameter(Mandatory = $true, ParameterSetName = "Cred")]
         [ValidateSet("UserPassword","SecretKey")]
-        [string] $Type
+        [string] $Type,
+
+        [Parameter(Mandatory = $true, ParameterSetName = "Plain")]
+        [string] $UserName,
+
+        [Parameter(Mandatory = $true, ParameterSetName = "Plain")]
+        [string] $SecretKey
+
     )
 
     if (-not $BAPISession.ApiUrl) {
@@ -11,37 +21,54 @@ function Invoke-BAPIAuthentication {
         return
     }
 
-    switch ($Type) {
-        "UserPassword" {              
-            try {                
-                $url = "$($BAPISession.ApiUrl)authuser/$($Credential.UserName)/"
-                $body =  @{
-                    UserName = $Credential.UserName
-                    Password = $Credential.GetNetworkCredential().Password
-                } | ConvertTo-Json
-                Write-Verbose "Invoking $url"
-                $auth = Invoke-RestMethod $url -Method Post -Body $body -ContentType "application/json"
-                if ($auth) {
-                    $secretKey = $auth.SecretKey
+    if ($Credential){
+        Write-Verbose "Authentication by credential"
+        $UserName = $Credential.UserName
+        switch ($Type) {
+            "UserPassword" {              
+                try {                
+                    $url = "$($BAPISession.ApiUrl)/authuser/$($Credential.UserName)/"
+                    $body =  @{
+                        UserName = $Credential.UserName
+                        Password = $Credential.GetNetworkCredential().Password
+                    } | ConvertTo-Json
+                    Write-Verbose "Invoking $url"
+                    $auth = Invoke-RestMethod $url -Method Post -Body $body -ContentType "application/json"
+                    if ($auth) {
+                        $SecretKey = $auth.SecretKey
+                        $BAPISession.UserId = $auth.UserId
+    
+                    }
+                    Write-Verbose ($auth | ConvertTo-Json)         
                 }
-                Write-Verbose ($auth | ConvertTo-Json)         
+                catch {
+                    Write-Warning "Error while authentication to API`n$_"        
+                }
             }
-            catch {
-                Write-Warning "Error while authentication to API`n$_"        
-            }
+            "SecretKey" {
+                $SecretKey = $Credential.GetNetworkCredential().Password
+            }        
+            Default {}
         }
-        "SecretKey" {
-            $secretKey = $Credential.GetNetworkCredential().Password
-        }        
-        Default {}
     }
 
-    if ($secretKey) {
-        $base64 = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$($UserCredential.UserName):$secretKey"))    
+    if ($SecretKey) {
+        $base64 = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$UserName`:$SecretKey"))    
         Write-Verbose "Adding to headers [Basic $base64]" 
         $BAPISession.Headers["Authorization"] = "Basic $base64"
     }
     else {
         Write-Warning "API Authentication failed"
+        return
+    }
+
+    # try to get UserId if using secretkey auth
+    if (-not $BAPISession.UserId) {
+        Write-Verbose "Getting userid for [$UserName]"
+        $user = Get-BapiUser -UserId "me"
+        if ($user) {
+            Write-Verbose "UserId [$($user.Id)]"
+            $BAPISession.UserId = $user.Id
+        }
     }
 }
--- PSBeneAPI/Public/Connect-BAPISession.ps1
+++ PSBeneAPI/Public/Connect-BAPISession.ps1
@@ -1,4 +1,9 @@
-function Connect-BAPISession {
+function ValidateUri($address) {
+	$uri = $address -as [System.URI]
+	return (($null -ne $uri.AbsoluteURI ) -and ($uri.Scheme -in "http","https"))
+}
+
+function Connect-BapiSession {
     [CmdLetBinding()]
     param(        
         #API user credentials. Passowrd is user password
@@ -6,7 +11,7 @@ function Connect-BAPISession {
         #API secretkey credentials. Passwor is SecretKey
         [pscredential]$APICredential,
         #API Url. If given, discovery will not be done
-        [uri] $ApiUrl
+        [String] $ApiUrl
     )
 
     #Figure out api username
@@ -17,23 +22,46 @@ function Connect-BAPISession {
         $APICredential.UserName            
     }
     else {
-        $env:BENEAPI_USERNAME
+        #env variable BENEAPI_SECRETKEY
+        if ($env:BENEAPI_USERNAME) {
+            $env:BENEAPI_USERNAME
+        }
+        else {
+            Write-Warning "Cannot figure out username!"
+            return
+        }
+        
     }
 
     #should do discovery or not?
     if ($APIUrl) {
-       Write-Verbose "using [$ApiUrl] not doing discovery"
-        $BAPISession.ApiUrl = $ApiUrl
+        if (ValidateUri($APIUrl)) {
+            Write-Verbose "using [$ApiUrl] not doing discovery"
+            $BAPISession.ApiUrl = $ApiUrl -replace "/$",""
+        }
+        else {
+            Write-Warning "Not valid Url [$ApiUrl]"
+        }
     }
     else {
-        Get-BAPIUrl
+        Get-BapiUrl
     }
 
     #Should do userauth?
     if ($UserCredential) {
-        Invoke-BAPIAuthentication $UserCredential -Type UserPassword
+        Invoke-BapiAuthentication -Credential $UserCredential -Type UserPassword
     }
-    if ($APICredential) {
-        Invoke-BAPIAuthentication $APICredential -Type SecretKey
+    elseif ($APICredential) {
+        Invoke-BapiAuthentication -Credential $APICredential -Type SecretKey
+    }
+    else {
+        if (-not $env:BENEAPI_SECRETKEY) {
+            Write-Warning "no secretkey"
+            return
+        }
+        else {
+            Invoke-BapiAuthentication -UserName $env:BENEAPI_USERNAME -SecretKey $env:BENEAPI_SECRETKEY
+        }
+        #env variable BENEAPI_SECRETKEY
     }
 }
